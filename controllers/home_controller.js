cc7e8d7ef38f04e95ba08436050d423f817fad58
AC VERMA <acverma@ACs-MacBook-Air.local>
2022-06-03 22:08:01 +0530

    render first page using ejs

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,9 @@
-module.exports.home=function(req,res){
-    return res.end('<h1>Express is up for MySpace </h1>');
-};
+ module.exports.home=function(req,res){
+     return res.render('home',{
+         title:"Avanindra"
+     });
+ };
 
 module.exports.logout=function(req,res){
     return res.end('<h1>Successfully Logged out!</h1>');
-}
+};
